471a610e1a67a9ff185aaac9837d56b55761c4bd
Jose Luis Guevara <jose.guevara@bo.truextend.com>
2018-03-15 20:09:44 -0400

    Initiating vidly project mvc pattern

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Ramdom
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Itachi Uchiha!" };
+            //return View(movie);
+            //return HttpNotFound();
+            //return Content("Hello world!");
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page=1, sortBy="name" });
+        }
+    }
+}
